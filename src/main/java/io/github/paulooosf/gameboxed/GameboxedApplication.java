d523a3cde68205838639a70f01aa18b602f09182
paulooosf <phpessoal2021@gmail.com>
2024-12-15 16:05:56 -0300

    feat: Primeiro commit

---
+++ src/main/java/io/github/paulooosf/gameboxed/GameboxedApplication.java
@@ -0,0 +1,13 @@
+package io.github.paulooosf.gameboxed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameboxedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameboxedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/paulooosf/gameboxed/GameboxedApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.paulooosf.gameboxed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameboxedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
