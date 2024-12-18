a4c4cd9d0e71104c0140145c6a442786bc0eea9f
paulooosf <phpessoal2021@gmail.com>
2024-12-17 23:56:16 -0300

    feat: Estruturação inicial da classe Jogo

---
+++ src/main/java/io/github/paulooosf/gameboxed/model/Jogo.java
@@ -0,0 +1,135 @@
+package io.github.paulooosf.gameboxed.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "JOGO")
+public class Jogo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "jog_cd_id")
+    private Long id;
+
+    @Column(name = "jog_txt_nome", length = 100, nullable = false)
+    private String nome;
+    @Column(name = "jog_txt_descricao", length = 500, nullable = false)
+    private String descricao;
+
+    @Column(name = "jog_txt_empresa", length = 50, nullable = false)
+    private String empresa;
+
+    @Column(name = "jog_int_ano", nullable = false)
+    private Integer ano;
+
+    @Column(name = "jog_dp_nota")
+    private Double nota;
+
+    @Column(name = "jog_int_quantidade_avaliacoes", nullable = false)
+    private Integer quantidadeAvaliacoes;
+
+    @Column(name = "jog_txt_link_capa", nullable = false)
+    private String linkCapa;
+
+    @Column(name = "jog_txt_link_banner", nullable = false)
+    private String linkBanner;
+
+    @Column(name = "jog_txt_link_trailer")
+    private String linkTrailer;
+
+    public Jogo() {
+    }
+
+    public Jogo(Long id, String nome, String descricao, String empresa, Integer ano, Double nota, Integer quantidadeAvaliacoes,
+                String linkCapa, String linkBanner, String linkTrailer) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.empresa = empresa;
+        this.ano = ano;
+        this.nota = nota;
+        this.quantidadeAvaliacoes = quantidadeAvaliacoes;
+        this.linkCapa = linkCapa;
+        this.linkBanner = linkBanner;
+        this.linkTrailer = linkTrailer;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome() {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+
+    public Double getNota() {
+        return nota;
+    }
+
+    public void setNota(Double nota) {
+        this.nota = nota;
+    }
+
+    public Integer getQuantidadeAvaliacoes() {
+        return quantidadeAvaliacoes;
+    }
+
+    public void setQuantidadeAvaliacoes(Integer quantidadeAvaliacoes) {
+        this.quantidadeAvaliacoes = quantidadeAvaliacoes;
+    }
+
+    public String getLinkCapa() {
+        return linkCapa;
+    }
+
+    public void setLinkCapa(String linkCapa) {
+        this.linkCapa = linkCapa;
+    }
+
+    public String getLinkBanner() {
+        return linkBanner;
+    }
+
+    public void setLinkBanner(String linkBanner) {
+        this.linkBanner = linkBanner;
+    }
+
+    public String getLinkTrailer() {
+        return linkTrailer;
+    }
+
+    public void setLinkTrailer(String linkTrailer) {
+        this.linkTrailer = linkTrailer;
+    }
+}
