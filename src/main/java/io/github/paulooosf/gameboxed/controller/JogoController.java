e9c18bd8504cf7ca030800f6d5367b6fd2dd3514
paulooosf <phpessoal2021@gmail.com>
2025-01-05 17:43:21 -0300

    feat: estruturação inicial do controller de Jogo

---
+++ src/main/java/io/github/paulooosf/gameboxed/controller/JogoController.java
@@ -0,0 +1,52 @@
+package io.github.paulooosf.gameboxed.controller;
+
+import io.github.paulooosf.gameboxed.dto.JogoListarDTO;
+import io.github.paulooosf.gameboxed.dto.JogoSaidaDTO;
+import io.github.paulooosf.gameboxed.model.Jogo;
+import io.github.paulooosf.gameboxed.service.JogoService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/jogo")
+public class JogoController {
+
+    @Autowired
+    private JogoService service;
+
+    @GetMapping("/lista")
+    public ResponseEntity<Page<JogoListarDTO>> listar (Pageable pageable) {
+        return ResponseEntity.ok(service.listar(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<JogoSaidaDTO> buscar(@PathVariable Long id) {
+        return ResponseEntity.ok(service.buscar(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<JogoSaidaDTO> cadastrar(@Valid @RequestBody Jogo jogo) {
+        JogoSaidaDTO jogoDTO = service.cadastrar(jogo);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(jogo.getId()).toUri();
+        return ResponseEntity.created(uri).body(jogoDTO);
+    }
+
+    @PutMapping("/editar/{id}")
+    public ResponseEntity<JogoSaidaDTO> editar(@PathVariable Long id, @Valid @RequestBody Jogo jogo) {
+        return ResponseEntity.ok(service.editar(id, jogo));
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+        service.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
+}
