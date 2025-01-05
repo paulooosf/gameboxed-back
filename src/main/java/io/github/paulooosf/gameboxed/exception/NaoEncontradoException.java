88ceb17379cc837c99dfacd1a3bff113f5ab2aaa
paulooosf <phpessoal2021@gmail.com>
2025-01-05 17:04:31 -0300

    feat: criando exception customizada

---
+++ src/main/java/io/github/paulooosf/gameboxed/exception/NaoEncontradoException.java
@@ -0,0 +1,8 @@
+package io.github.paulooosf.gameboxed.exception;
+
+public class NaoEncontradoException extends RuntimeException {
+
+    public NaoEncontradoException(String mensagem) {
+        super(mensagem);
+    }
+}
