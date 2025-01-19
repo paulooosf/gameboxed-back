6d60650f66f833d6b9d2c1bf22dfedd0703cee70
paulooosf <phpessoal2021@gmail.com>
2025-01-19 00:25:18 -0300

    feat: adicionando sobrescrita do método findAll no UsuarioRepository

--- src/main/java/io/github/paulooosf/gameboxed/repository/UsuarioRepository.java
+++ src/main/java/io/github/paulooosf/gameboxed/repository/UsuarioRepository.java
@@ -1,10 +1,15 @@
 package io.github.paulooosf.gameboxed.repository;
 
 import io.github.paulooosf.gameboxed.model.Usuario;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    public Page<Usuario> findAll(Pageable pageable);
+
     public Optional<Usuario> findById(Long id);
 }
