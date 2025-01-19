71f36d1a12367e32ad336e97c751a81eb45525a5
paulooosf <phpessoal2021@gmail.com>
2025-01-19 19:04:29 -0300

    feat: atualizando DTO de saída de jogo para exibir as avaliações.

--- src/main/java/io/github/paulooosf/gameboxed/dto/JogoSaidaDTO.java
+++ src/main/java/io/github/paulooosf/gameboxed/dto/JogoSaidaDTO.java
@@ -4,6 +4,8 @@ import io.github.paulooosf.gameboxed.model.Jogo;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+import java.util.List;
+
 public record JogoSaidaDTO(@NotNull(message = "ID Não preenchido!") Long id,
                            @NotBlank(message = "Nome não preenchido!") String nome,
                            @NotBlank(message = "Descrição não preenchida!") String descricao,
@@ -13,10 +15,15 @@ public record JogoSaidaDTO(@NotNull(message = "ID Não preenchido!") Long id,
                            @NotNull(message = "Quantidade de avaliações não preenchida!") Integer quantidadeAvaliacoes,
                            @NotBlank(message = "Link da capa não preenchido!") String linkCapa,
                            @NotBlank(message = "Link do banner não preenchido!") String linkBanner,
-                           @NotBlank(message = "Link do trailer não preenchido!") String linkTrailer
+                           @NotBlank(message = "Link do trailer não preenchido!") String linkTrailer,
+                           @NotNull(message = "Lista de avaliações não preenchida!") List<AvaliacaoSaidaDTO> avaliacoes
                            ) {
     public JogoSaidaDTO(Jogo jogo) {
         this(jogo.getId(), jogo.getNome(), jogo.getDescricao(), jogo.getEmpresa(), jogo.getAno(), jogo.getNota(),
-                jogo.getQuantidadeAvaliacoes(), jogo.getLinkCapa(), jogo.getLinkBanner(), jogo.getLinkTrailer());
+                jogo.getQuantidadeAvaliacoes(), jogo.getLinkCapa(), jogo.getLinkBanner(), jogo.getLinkTrailer(),
+                jogo.getAvaliacoes()
+                        .stream()
+                        .map(AvaliacaoSaidaDTO::new)
+                        .toList());
     }
 }
