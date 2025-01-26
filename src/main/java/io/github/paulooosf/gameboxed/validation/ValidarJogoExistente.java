ec75eef8d2b7960f909b464b0b4f56493f6a6b1f
paulooosf <phpessoal2021@gmail.com>
2025-01-26 14:43:08 -0300

    feat: removendo dependências desnecessárias nas validações

--- src/main/java/io/github/paulooosf/gameboxed/validation/ValidarJogoExistente.java
+++ src/main/java/io/github/paulooosf/gameboxed/validation/ValidarJogoExistente.java
@@ -2,8 +2,6 @@ package io.github.paulooosf.gameboxed.validation;
 
 import io.github.paulooosf.gameboxed.exception.NaoEncontradoException;
 import io.github.paulooosf.gameboxed.model.Jogo;
-import io.github.paulooosf.gameboxed.repository.JogoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -11,8 +9,8 @@ import java.util.Optional;
 @Component
 public class ValidarJogoExistente {
 
-    @Autowired
-    private JogoRepository repository;
+    private ValidarJogoExistente() {
+    }
 
     public static void validar(Optional<Jogo> jogoOpt) {
         if (jogoOpt.isEmpty()) {
