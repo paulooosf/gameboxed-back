caf60f730524a05ad31b37b08ccfd6a6f76b9969
paulooosf <phpessoal2021@gmail.com>
2025-01-26 14:41:48 -0300

    feat: criando exception de geração de token

---
+++ src/main/java/io/github/paulooosf/gameboxed/exception/GeracaoTokenException.java
@@ -0,0 +1,8 @@
+package io.github.paulooosf.gameboxed.exception;
+
+public class GeracaoTokenException extends RuntimeException {
+
+    public GeracaoTokenException(String mensagem) {
+        super(mensagem);
+    }
+}
