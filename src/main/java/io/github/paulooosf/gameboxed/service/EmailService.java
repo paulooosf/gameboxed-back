7c724b6866154bcd78f873bad5a302f5d6e75468
Paulo Henrique <phpessoal2021@gmail.com>
2025-04-26 14:30:19 -0300

    feat: adicionando classe de envio de e-mail

---
+++ src/main/java/io/github/paulooosf/gameboxed/service/EmailService.java
@@ -0,0 +1,34 @@
+package io.github.paulooosf.gameboxed.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    @Autowired
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    @Value("${spring.mail.username}")
+    private String remetente;
+
+    public void enviarEmailTexto(String destinatario, String assunto, String mensagem) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(remetente);
+            message.setTo(destinatario);
+            message.setSubject(assunto);
+            message.setText(mensagem);
+            mailSender.send(message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
