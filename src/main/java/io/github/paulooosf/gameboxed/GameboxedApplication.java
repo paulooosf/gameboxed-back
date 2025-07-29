6b4a5516a8b608a1472c399be0aeefd03ba52327
Paulo Henrique <phpessoal2021@gmail.com>
2025-07-28 22:29:09 -0300

    config: habilitando redis no sistema

--- src/main/java/io/github/paulooosf/gameboxed/GameboxedApplication.java
+++ src/main/java/io/github/paulooosf/gameboxed/GameboxedApplication.java
@@ -2,8 +2,10 @@ package io.github.paulooosf.gameboxed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class GameboxedApplication {
 
 	public static void main(String[] args) {
