ba35ed5c06699b07c54a794af1d629b1bc90c9ab
Paulo Henrique <phpessoal2021@gmail.com>
2026-07-20 15:17:47 -0300

    feat: implementando ci/cd com GitHub Actions

--- src/test/java/io/github/paulooosf/gameboxed/GameboxedApplicationTests.java
+++ src/test/java/io/github/paulooosf/gameboxed/GameboxedApplicationTests.java
@@ -1,9 +1,7 @@
 package io.github.paulooosf.gameboxed;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class GameboxedApplicationTests {
 
 	@Test
